388d21d566800810d82b4cc2da77d7c02a1e5ce5
Md Ishtiyaque Ahmad <mda.ug19.cs@nitp.ac.in>
2022-07-21 21:42:01 +0530

    modified configuration

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     TESTING = False
     PORT =port
     SECRET_KEY = 'very_very-very_very Secret-Key'
-    SQLALCHEMY_DATABASE_URI="postgres://uootornqvinrxu:12c268b0a499dad621a5ded9c9214a1bc9a120b1a7988a0a6a496cf51002f48c@ec2-44-206-197-71.compute-1.amazonaws.com:5432/dfsoqjfus90nu"
+    SQLALCHEMY_DATABASE_URI="postgresql://uootornqvinrxu:12c268b0a499dad621a5ded9c9214a1bc9a120b1a7988a0a6a496cf51002f48c@ec2-44-206-197-71.compute-1.amazonaws.com:5432/dfsoqjfus90nu"
 
 class ProductionConfig(Config):
     DEBUG = False
